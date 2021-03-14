b42513c389b815ce9de9331e19a74e4a128e469c
Azat <58173417+tasadar23@users.noreply.github.com>
2021-03-14 23:24:03 +0500

    Визуализация таблицы сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
@@ -14,8 +15,21 @@ namespace WebStore.Controllers
         //    //return Content("Firest Controller Action");
         //    return View();
         //}
+
+        public static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LasttName = "Иванов", FirstName = "Иван", Patronymic = " Иванович", Age = 23 },
+            new Employee { Id = 1, LasttName = "Петров", FirstName = "Петр", Patronymic = " Петрович", Age = 31 },
+            new Employee { Id = 1, LasttName = "Сидоров", FirstName = "Сидр", Patronymic = " Сидорович", Age = 18 }
+
+        };
         public IActionResult Index() => View();
-        
+
         public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
+        }
     }
-} 
+}
