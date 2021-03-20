6e69e811b8f5babd1a6700fb96ab81ceb40184fc
Azat <58173417+tasadar23@users.noreply.github.com>
2021-03-21 00:32:12 +0500

    pause

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
