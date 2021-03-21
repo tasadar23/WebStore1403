b6db2e52df8537754a9f78a6b04b29d44d050783
Azat <58173417+tasadar23@users.noreply.github.com>
2021-03-21 11:54:47 +0500

    Контроллер сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        public IActionResult Index() => View();
+        public static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LasttName = "Иванов", FirstName = "Иван", Patronymic = " Иванович", Age = 23 },
+            new Employee { Id = 2, LasttName = "Петров", FirstName = "Петр", Patronymic = " Петрович", Age = 31 },
+            new Employee { Id = 3, LasttName = "Сидоров", FirstName = "Сидор", Patronymic = " Сидорович", Age = 18 }
+
+        };
+        public IActionResult Index() => View(_Employees);
     }
 }
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebStore.Models;
 
 namespace WebStore.Controllers
 {
@@ -16,20 +11,9 @@ namespace WebStore.Controllers
         //    return View();
         //}
 
-        public static readonly List<Employee> _Employees = new()
-        {
-            new Employee { Id = 1, LasttName = "Иванов", FirstName = "Иван", Patronymic = " Иванович", Age = 23 },
-            new Employee { Id = 1, LasttName = "Петров", FirstName = "Петр", Patronymic = " Петрович", Age = 31 },
-            new Employee { Id = 1, LasttName = "Сидоров", FirstName = "Сидр", Patronymic = " Сидорович", Age = 18 }
 
-        };
         public IActionResult Index() => View();
 
-        public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
-
-        public IActionResult Employees()
-        {
-            return View(_Employees);
-        }
+        public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");        
     }
 }
