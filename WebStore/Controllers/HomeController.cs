c8c8e0c10f16be48637d11fd10f98dca30664557
Azat <58173417+tasadar23@users.noreply.github.com>
2021-03-21 16:50:29 +0500

    Добавление оставшейся инфраструктуры вьюшек

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -17,5 +17,13 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");    
         
         public IActionResult Contact() => View();
+        public IActionResult Error404() => View();
+        public IActionResult Blog() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
     }
 }
