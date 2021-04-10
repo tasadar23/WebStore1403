2432c77cc63c1c816d170f7e8cf7fcf1e6c847d3
Azat <58173417+tasadar23@users.noreply.github.com>
2021-04-10 21:29:08 +0500

    Иетерфейс будущего сервиса

---
+++ WebStore/Infrastructure/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services.Interfaces
+{
+    public interface IEmployeesData
+    {
+        //стандартные 5 операций chroot interface
+        IEnumerable<Employee> Get();
+
+        Employee Get(int id);
+
+        int Add(Employee employee);
+
+        void Update(Empoyees empoyee);
+
+        bool Delete(int id);
+    }
+}
